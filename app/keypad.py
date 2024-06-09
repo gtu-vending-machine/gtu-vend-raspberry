ca0d2d55c02bfc5f7052ed9efa80c7f68ad322e4
Yusuf Arslan <yusufarsln98@outlook.com>
2024-06-09 18:55:51 +0300

    pinout

--- app/keypad.py
+++ app/keypad.py
@@ -1,20 +1,17 @@
 import RPi.GPIO as GPIO
 
 # Keypad configuration
-ROWS = [7, 13, 15, 29]
+ROWS = [7, 11, 13, 15]
 COLS = [16, 18, 22]
 
-KEYMAP = [
-    ['1', '2', '3'],
-    ['4', '5', '6'],
-    ['7', '8', '9'],
-    ['*', '0', '#']
-]
+KEYMAP = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["*", "0", "#"]]
+
 
 def setup_keypad():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(ROWS, GPIO.OUT, initial=GPIO.HIGH)
     GPIO.setup(COLS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 def get_key():
     key = None
@@ -27,6 +24,7 @@ def get_key():
                     pass
         GPIO.output(ROWS[i], GPIO.HIGH)
     return key
+
 
 def destroy():
     GPIO.cleanup()
